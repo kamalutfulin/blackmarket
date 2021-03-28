eb2885f937120853cd06a2bade57e5e56a8db0e6
kamalutfulin <kamalutfulin@gmail.com>
2021-03-28 18:41:51 +0300

    remode 13

--- src/servlets/IndexServlet.java
+++ src/servlets/IndexServlet.java
@@ -8,13 +8,14 @@ import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
 
-//@WebServlet(urlPatterns = {"/index"})
+@WebServlet(urlPatterns = {"/index"})
 public class IndexServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
